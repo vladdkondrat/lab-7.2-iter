b00a8c0d3807c6bbbff7031f5588c3029d30ed1d
kondratvlad <vladkondrat87@gmail.com>
2024-10-28 20:33:10 +0200

    first master commit

---
+++ UnitTest 74/UnitTest 74.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 7.2 iter/lab 7.2 iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest74
+{
+	TEST_CLASS(UnitTest74)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+            int rows = 3, cols = 3;
+            int** matrix = new int* [rows];
+            for (int i = 0; i < rows; ++i) {
+                matrix[i] = new int[cols];
+            }
+
+            matrix[0][0] = 4; matrix[0][1] = 5; matrix[0][2] = 6;
+            matrix[1][0] = 2; matrix[1][1] = 8; matrix[1][2] = 3;
+            matrix[2][0] = 1; matrix[2][1] = 2; matrix[2][2] = 4;
+
+            int minEven;
+            bool found = SearchMinEven(matrix, rows, minEven);
+
+            Assert::IsTrue(found);
+            Assert::AreEqual(2, minEven); // Мінімальний парний елемент
+
+            for (int i = 0; i < rows; ++i) {
+                delete[] matrix[i];
+            }
+            delete[] matrix;
+		}
+	};
+}
---
+++ lab 7.2 iter.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+// Функція для створення матриці
+void Create(int** a, const int n, const int Low, const int High) {
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            a[i][j] = Low + rand() % (High - Low + 1);
+        }
+    }
+}
+
+// Функція для друку матриці
+void Print(int** a, const int n) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << setw(4) << a[i][j];
+        }
+        cout << endl;
+    }
+}
+
+// Функція для пошуку мінімального парного елементу
+bool SearchMinEven(int** a, const int n, int& minEven) {
+    bool found = false;
+    for (int j = 0; j < n; j += 2) { // перевіряємо лише парні стовпці
+        for (int i = 0; i < n; ++i) {
+            if (a[i][j] % 2 == 0) {
+                if (!found || a[i][j] < minEven) {
+                    minEven = a[i][j];
+                    found = true;
+                }
+            }
+        }
+    }
+    return found;
+}
+
+int main() {
+    int Low = 7;
+    int High = 65;
+    int n;
+
+    cout << "n = ";
+    cin >> n;
+
+    int** a = new int* [n];
+    for (int i = 0; i < n; i++)
+        a[i] = new int[n];
+
+    Create(a, n, Low, High);
+    cout << "Original matrix:" << endl;
+    Print(a, n);
+
+    int minEven;
+    if (SearchMinEven(a, n, minEven))
+        cout << "min even = " << minEven << endl;
+    else
+        cout << "there are no even elements" << endl;
+
+    for (int i = 0; i < n; i++)
+        delete[] a[i];
+    delete[] a;
+
+    return 0;
+}
